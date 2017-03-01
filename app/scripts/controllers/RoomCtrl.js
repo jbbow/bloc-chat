7d0802b33b925770c09ee1d4f15ec82f0f81bf0f
Jill Bowers <jill@camostudio.com>
2017-02-28 22:33:18 -0500

    List Chat Rooms

---
+++ app/scripts/controllers/RoomCtrl.js
@@ -0,0 +1,10 @@
+    
+(function() {
+    function RoomCtrl(Room) {
+        this.allRooms = Room.all;
+    }
+
+angular
+        .module('blocChat')
+        .controller('RoomCtrl', ['Room', RoomCtrl]);
+})();
--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,5 +1,22 @@
-angular
-    .module('blocChat', ['ui.router','firebase'])
-    .config(config);
+(function() {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+            enabled: true,
+            requireBase: false
+            });
 
- })();
+        $stateProvider
+            .state('room', {
+                url: '/',
+                controller: 'RoomCtrl as rooms',
+                templateUrl: '/templates/home.html'
+ 		             });  
+        
+    }
+
+    
+    angular
+        .module('blocChat', ['ui.router', 'firebase'])
+        .config(config);
+})();
